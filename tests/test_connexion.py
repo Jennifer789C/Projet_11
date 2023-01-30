3d637ce0557cb2c8b5595261c0d153d51fb40dfd
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-01-30 11:08:53 +0100

    tests fonction book ok

--- tests/test_connexion.py
+++ tests/test_connexion.py
@@ -1,15 +1,9 @@
+from flask import request, url_for
 from server import loadClubs, loadCompetitions
 
 
-def test_vue_index(client, templates_utilises):
-    reponse = client.get("/")
-    assert reponse.status_code == 200
-    assert len(templates_utilises) == 1
-    template, context = templates_utilises[0]
-    assert template.name == "index.html"
-
-
 def test_loadClubs():
+    """Test de la fonction loadClubs()"""
     resultat = [{"name": "Simply Lift",
                  "email": "john@simplylift.co",
                  "points": "13"},
@@ -24,6 +18,7 @@ def test_loadClubs():
 
 
 def test_loadCompetitions():
+    """Test de la fonction loadCompetitions()"""
     resultat = [{"name": "Spring Festival",
                  "date": "2020-03-27 10:00:00",
                  "numberOfPlaces": "25"},
@@ -34,7 +29,23 @@ def test_loadCompetitions():
     assert loadCompetitions() == resultat
 
 
+def test_vue_index(client, templates_utilises):
+    """L'URL / est accessible : status_code + template + context"""
+    reponse = client.get("/")
+    assert reponse.status_code == 200
+    assert len(templates_utilises) == 1
+    template, context = templates_utilises[0]
+    assert template.name == "index.html"
+
+
+def test_vue_index_inaccessible(client):
+    """Méthode POST non autorisée sur l'URL /"""
+    reponse = client.post("/")
+    assert reponse.status_code == 405
+
+
 def test_login_adresse_valide(client, templates_utilises, club, competitions):
+    """Le club se connecte avec son adresse mail + redirection vers l'URL /showSummary : status_code + template + context"""
     reponse = client.post("/showSummary", data={"email": "mail@test.com"}, follow_redirects=True)
     assert reponse.status_code == 200
     assert len(templates_utilises) == 1
@@ -45,6 +56,8 @@ def test_login_adresse_valide(client, templates_utilises, club, competitions):
 
 
 def test_login_adresse_invalide(client, templates_utilises, club):
+    """Le club essaie de se connecter avec une adresse mail invalide : un message d'erreur s'affiche et le club est invité à
+    modifier son adresse mail de connexion"""
     reponse = client.post("/showSummary", data={"email": "mail@test.fr"}, follow_redirects=True)
     assert reponse.status_code == 200
     assert len(templates_utilises) == 1
@@ -53,5 +66,15 @@ def test_login_adresse_invalide(client, templates_utilises, club):
 
 
 def test_vue_showSummary_inaccessible(client):
+    """Méthode GET non autorisée sur l'URL /showSummary"""
     reponse = client.get("/showSummary")
     assert reponse.status_code == 405
+
+
+def test_deconnexion(client, templates_utilises):
+    """Le club se déconnecte + redirection vers l'URL / : status_code + template + context"""
+    reponse = client.get("/logout", follow_redirects=True)
+    assert reponse.status_code == 200
+    assert len(templates_utilises) == 1
+    template, context = templates_utilises[0]
+    assert template.name == "index.html"
---
+++ tests/test_echange_points.py
@@ -0,0 +1,88 @@
+def test_vue_book(client, templates_utilises, club, competitions):
+    """Test de la vue si club et compétition ok : status_code + template + context"""
+    club = club[0]
+    competition = competitions[0]
+    uri = "/book/" + competition["name"] + "/" + club["name"]
+    reponse = client.get(uri, follow_redirects=True)
+    assert reponse.status_code == 200
+    assert len(templates_utilises) == 1
+    template, context = templates_utilises[0]
+    assert template.name == "booking.html"
+    assert context["club"] == club
+    assert context["competition"] == competition
+
+
+def test_vue_book_club_errone(client, templates_utilises, competitions):
+    """Test de la vue si le club est erroné : status_code + template + context"""
+    competition = competitions[0]
+    uri = "/book/" + competition["name"] + "/Club erreur"
+    reponse = client.get(uri, follow_redirects=True)
+    assert reponse.status_code == 200
+    assert len(templates_utilises) == 1
+    template, context = templates_utilises[0]
+    assert template.name == "welcome.html"
+    assert context["club"] == "Club erreur"
+    assert context["competitions"] == competitions
+
+
+def test_vue_book_competition_erronnee(client, templates_utilises, club, competitions):
+    """Test de la vue si la compétition est erronée : status_code + template + context"""
+    club = club[0]["name"]
+    uri = "/book/Competition erreur/" + club
+    reponse = client.get(uri, follow_redirects=True)
+    assert reponse.status_code == 200
+    assert len(templates_utilises) == 1
+    template, context = templates_utilises[0]
+    assert template.name == "welcome.html"
+    assert context["club"] == club
+    assert context["competitions"] == competitions
+
+
+def test_vue_book_inaccessible(client):
+    """Méthode POST non autorisée sur l'URL /book/<competition>/<club>"""
+    reponse = client.post("book/<competition>/<club>")
+    assert reponse.status_code == 405
+
+
+def test_reservation():
+    """Saisie d'une valeur pour réserver"""
+    pass
+
+
+def test_reservation_limite_points():
+    """Le club ne peut pas réserver plus de places qu'il n'a de points"""
+    pass
+
+
+def test_reservation_limite_places():
+    """Le club ne peut pas réserver plus de places que la compétition en a de disponible"""
+    pass
+
+
+def test_reservation_12_max():
+    """Le club ne peut pas réserver plus de 12 places par compétition"""
+    pass
+
+
+def test_vue_puchasePlaces_inaccessible():
+    """Méthode GET non autorisée sur l'URL /purchasePlaces"""
+
+
+def test_bouton_reservation_inaccessible():
+    """Le bouton de réservation ne doit pas s'afficher si :
+    - il n'y a plus de place disponible dans la compétition
+    - le club n'a plus de point disponible
+    - le club a déjà dépensé 12 points dans cette compétition
+    - la date de la compétition est antérieure à la date du jour
+    """
+    pass
+
+
+def test_points_disponible():
+    """Le nombre de points du club doit être à jour"""
+    pass
+
+
+def test_places_disponible():
+    """Le nombre de places disponibles de la compétition doit être à jour"""
+    pass
